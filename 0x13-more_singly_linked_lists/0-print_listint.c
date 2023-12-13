6be46095175ad129507a055afc71cf542ad7237c
Meatpie.1 <kpakpoazaria6@gmail.com>
2023-12-13 10:40:23 -0800

    kpakpo

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,14 +9,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	 size_t num = 0;
+size_t num = 0;
 
-	 while (h)
-	 {
-		 num++;
-		 printf("%d\n", h->n);
-		 h = h->next;
-	 }
+while (h)
+{
+num++;
+printf("%d\n", h->n);
+h = h->next;
+}
 
-	 return (num);
+return (num);
 }
